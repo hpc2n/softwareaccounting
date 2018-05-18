7fd8466aed8cd0893626de55a638aea8ad8826aa
Magnus Jonsson <magnus@hpc2n.umu.se>
2018-05-18 14:36:11 +0200

    Updated documentation.

--- sams/output/File.py
+++ sams/output/File.py
@@ -1,4 +1,25 @@
+"""
+Write outputs into the file system
 
+Config Options:
+[sams.output.File]
+  # Path where the output files are written.
+  base_path: /scratch/softwareaccounting/data
+
+  # Write files in file pattern. 
+  # Available data for replace is: jobid & node
+  file_pattern: "%(jobid)s.%(node)s.json"
+
+  # "Hash" the output based on --jobid / jobid_hash_size
+  jobid_hash_size: 1000
+
+  # If set uses the sysfsuid() syscall to write files as uid.
+  # This does not work on lustre.
+  # write_as_uid: 2066
+
+  # Skip the list of modules.
+  exclude: ['sams.sampler.ModuleName']
+"""
 import sams.base
 import time
 import os
