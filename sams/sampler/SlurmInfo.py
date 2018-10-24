1eff733c1baf2dfd07eccae830cd0cad1e41e8c1
Magnus Jonsson <magnus@hpc2n.umu.se>
2018-10-24 13:20:04 +0200

    Updated to use subprocess.Popen instead of os.popen
    Also adds support for extra environment variables for running command.

--- sams/sampler/SlurmInfo.py
+++ sams/sampler/SlurmInfo.py
@@ -10,6 +10,10 @@ sams.sampler.SlurmInfo:
     # path to scontrol command
     scontrol: /usr/local/bin/scontrol
 
+    # extra environments for command
+    environment:
+      PATH: "/bin:/usr/bin"
+
 Output:
 {
     account: "",
@@ -22,6 +26,7 @@ Output:
 import logging
 import os
 import re
+import subprocess
 
 import sams.base
 
@@ -54,38 +59,42 @@ class Sampler(sams.base.Sampler):
         command = COMMAND % (scontrol,jobid)
 
         try:
-            process = os.popen(command)
+            local_env = os.environ.copy()
+            for env,value in self.config.get([self.id,'environment'],{}).items():
+                local_env[env] = value
+            process = subprocess.Popen(command,env=local_env,shell=True,stdout=subprocess.PIPE).stdout
             data = process.readlines()
         except Exception as e:
+            logger.exception(e)
             logger.debug("Fail to run: %s, will try again in a while",command)
             # Try again next time :-)
             return
-
-        data[0].strip()
+        
+        data = data[0].decode().strip()
 
         # Find account in string
-        account = re.search(r'Account=([^ ]+)',data[0])
+        account = re.search(r'Account=([^ ]+)',data)
         if account:
             self.data['account'] = account.group(1)
 
         # Find username/uid in string\((\d+)\)
-        userid = re.search(r'UserId=([^\(]+)\((\d+)\)',data[0])
+        userid = re.search(r'UserId=([^\(]+)\((\d+)\)',data)
         if userid:
             self.data['username'] = userid.group(1)
             self.data['uid'] = userid.group(2)
 
         # Find username/uid in string
-        nodes = re.search(r'NumNodes=(\d+)',data[0])
+        nodes = re.search(r'NumNodes=(\d+)',data)
         if nodes:
             self.data['nodes'] = nodes.group(1)
 
         # Find username/uid in string
-        cpus = re.search(r'NumCPUs=(\d+)',data[0])
+        cpus = re.search(r'NumCPUs=(\d+)',data)
         if cpus:
             self.data['cpus'] = cpus.group(1)
 
         # Find StartTime
-        starttime = re.search(r'StartTime=(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d)',data[0])
+        starttime = re.search(r'StartTime=(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d)',data)
         if starttime:
             self.data['starttime'] = starttime.group(1)
 
