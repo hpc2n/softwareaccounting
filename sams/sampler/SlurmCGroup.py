1a516c90c0a98a93b1f8a8535bbfe330febd8b7a
Magnus Jonsson <magnus@hpc2n.umu.se>
2020-11-25 16:35:02 +0100

    Cleanup (isort, black, pylint)

--- sams/sampler/SlurmCGroup.py
+++ sams/sampler/SlurmCGroup.py
@@ -18,19 +18,21 @@ Output:
 }
 """
 
-import sams.base
+import logging
 import os
 import re
 
-import logging
+import sams.base
+
 logger = logging.getLogger(__name__)
 
+
 class Sampler(sams.base.Sampler):
-    def __init__(self,id,outQueue,config):
-        super(Sampler,self).__init__(id,outQueue,config)
+    def __init__(self, id, outQueue, config):
+        super(Sampler, self).__init__(id, outQueue, config)
         self.processes = {}
         self.cgroup = None
-        self.cgroup_base = self.config.get([self.id,"cgroup_base"],'/cgroup')
+        self.cgroup_base = self.config.get([self.id, "cgroup_base"], "/cgroup")
 
     def do_sample(self):
         return self._get_cgroup()
@@ -38,17 +40,19 @@ class Sampler(sams.base.Sampler):
     def sample(self):
         logger.debug("sample()")
 
-        cpus = self._cpucount(self.read_cgroup('cpuset','cpuset.cpus'))
-        memory_usage = self.read_cgroup('memory','memory.usage_in_bytes')
-        memory_limit = self.read_cgroup('memory','memory.limit_in_bytes')
-        memory_max_usage = self.read_cgroup('memory','memory.max_usage_in_bytes')
+        cpus = self._cpucount(self.read_cgroup("cpuset", "cpuset.cpus"))
+        memory_usage = self.read_cgroup("memory", "memory.usage_in_bytes")
+        memory_limit = self.read_cgroup("memory", "memory.limit_in_bytes")
+        memory_max_usage = self.read_cgroup("memory", "memory.max_usage_in_bytes")
 
-        self.store({
-                'cpus' : cpus,
-                'memory_usage': memory_usage,
-                'memory_limit': memory_limit,
-                'memory_max_usage': memory_max_usage,
-            })
+        self.store(
+            {
+                "cpus": cpus,
+                "memory_usage": memory_usage,
+                "memory_limit": memory_limit,
+                "memory_max_usage": memory_max_usage,
+            }
+        )
 
     def _get_cgroup(self):
         """ Get the cgroup base path for the slurm job """
@@ -56,35 +60,41 @@ class Sampler(sams.base.Sampler):
             return True
         for pid in self.pids:
             try:
-                with open("/proc/%d/cpuset" % pid,"r") as file:
+                with open("/proc/%d/cpuset" % pid, "r") as file:
                     cpuset = file.readline()
-                    m = re.search(r'^/(slurm/uid_\d+/job_\d+)/',cpuset)
+                    m = re.search(r"^/(slurm/uid_\d+/job_\d+)/", cpuset)
                     if m:
                         self.cgroup = m.group(1)
                         return True
             except Exception as e:
                 logger.debug("Failed to fetch cpuset for pid: %d", self.pids[0])
+                logger.debug(e)
         return False
 
-    def _cpucount(self,count):
+    def _cpucount(self, count):
         """ Calculate number of cpus from a "N,N-N"-structure """
         cpu_count = 0
         for c in count.split(","):
-            m = re.search(r'^(\d+)-(\d+)$',c)
+            m = re.search(r"^(\d+)-(\d+)$", c)
             if m:
-                cpu_count += int(m.group(2))-int(m.group(1))+1
-            m = re.search(r'^(\d+)$',c)
+                cpu_count += int(m.group(2)) - int(m.group(1)) + 1
+            m = re.search(r"^(\d+)$", c)
             if m:
                 cpu_count += 1
         return cpu_count
 
-    
-    def read_cgroup(self,type,id):
+    def read_cgroup(self, type, id):
         try:
-            with open(os.path.join(self.cgroup_base,type,self.cgroup,id),"r") as file:
+            with open(
+                os.path.join(self.cgroup_base, type, self.cgroup, id), "r"
+            ) as file:
                 return file.readline().strip()
         except IOError as err:
-            logger.debug("Failed to open %s for reading",os.path.join(self.cgroup_base,type,self.cgroup,id))
+            logger.debug(
+                "Failed to open %s for reading",
+                os.path.join(self.cgroup_base, type, self.cgroup, id),
+            )
+            logger.debug(err)
             return ""
 
     def final_data(self):
