975bc6367bde6fa72588a1271d8a12b027613568
Magnus Jonsson <magnus@hpc2n.umu.se>
2022-01-25 15:40:33 +0100

    Base modules updated with isort/black/pylint.

--- sams/base.py
+++ sams/base.py
@@ -18,153 +18,168 @@ You should have received a copy of the GNU General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 """
 
-import time
-
 import threading
+import time
 
 try:
     import queue
 except ImportError:
     import Queue as queue
 
-
 import logging
+
 logger = logging.getLogger(__name__)
 
-class PIDFinder(object):
+
+class PIDFinder:
     """ PIDFinder base class """
 
-    def __init__(self,id,jobid,config):
+    def __init__(self, id, jobid, config):
         self.id = id
         self.jobid = jobid
         self.config = config
 
+    # pylint: disable=no-self-use
     def find(self):
         raise Exception("Not implemented")
         # return []
 
+
 class Sampler(threading.Thread):
     """ Sampler base class """
 
-    def __init__(self,id,outQueue,config):
-        super(Sampler,self).__init__()
+    def __init__(self, id, outQueue, config):
+        super(Sampler, self).__init__()
         self.id = id
         self.outQueue = outQueue
         self.config = config
-        self.jobid = self.config.get(['options','jobid'])
+        self.jobid = self.config.get(["options", "jobid"])
         self.pidQueue = queue.Queue()
         self.pids = []
-        self.sampler_interval = self.config.get([self.id,"sampler_interval"],60)
+        self.sampler_interval = self.config.get([self.id, "sampler_interval"], 60)
 
-    def init(self):
+    def init(self):  # pylint: disable=no-self-use
         pass
 
     def run(self):
         try:
             self.init()
-        except Exception as e:
-            logger.exception("Failed to do self.init in %s" % self.id,e)
+        except Exception:
+            logger.exception("Failed to do self.init in %s", self.id)
         while True:
             try:
-                pids = self.pidQueue.get(timeout = self.sampler_interval)
+                pids = self.pidQueue.get(timeout=self.sampler_interval)
                 if not pids:
                     self.pidQueue.task_done()
                     break
                 logger.debug("Received new pids: %s", pids)
                 self.pids.extend(pids)
                 self.pidQueue.task_done()
-            except queue.Empty as e:
-                logger.debug("%s queue.Empty timeout" % self.id)
-                pass
+            except queue.Empty:
+                logger.debug("%s queue.Empty timeout", self.id)
             try:
                 if self.do_sample():
                     self.sample()
-            except Exception as e:
-                logger.exception("Failed to do self.sample in %s" % self.id,e)
-            
+            except Exception:
+                logger.exception("Failed to do self.sample in %s", self.id)
+
         try:
-            self.store(self.final_data(),'final')
-        except Exception as e:
-            logger.exception("Failed to do self.final_data in %s" % self.id,e)
+            self.store(self.final_data(), "final")
+        except Exception:
+            logger.exception("Failed to do self.final_data in %s", self.id)
         self.outQueue.join()
-    
-    def store(self,data,type='now'):
-        self.outQueue.put({
-                'id': self.id,
-                'data': data,
-                'type': type
-            })
+
+    def store(self, data, type="now"):
+        self.outQueue.put({"id": self.id, "data": data, "type": type})
 
     # this should be implemented in the real Sampler..
+    # pylint: disable=no-self-use
     def sample(self):
+        raise Exception("Not implemented")
+
+    # this should be implemented in the real Sampler..
+    # pylint: disable=no-self-use
+    def final_data(self):
         raise Exception("Not implemented")
 
     def do_sample(self):
         return len(self.pids) > 0
 
     def exit(self):
-        logger.debug("%s exit" % self.id)
+        logger.debug("%s exit", self.id)
         self.pidQueue.put(None)
 
-class Aggregator(object):
+
+class Aggregator:
     """ Aggregator base class """
 
-    def __init__(self,id,config):
+    def __init__(self, id, config):
         self.id = id
         self.config = config
 
-    def aggregate(self,data):
+    # pylint: disable=no-self-use
+    def aggregate(self, data):
         raise Exception("Not implemented")
 
-class Loader(object):
+
+class Loader:
     """ Loader base class """
 
-    def __init__(self,id,config):
+    def __init__(self, id, config):
         self.id = id
-        self.config = config        
+        self.config = config
 
+    # pylint: disable=no-self-use
     def load(self):
         raise Exception("Not implemented")
-    
+
+    # pylint: disable=no-self-use
     def next(self):
         raise Exception("Not implemented")
 
+    # pylint: disable=no-self-use
     def commit(self):
         raise Exception("Not implemented")
 
-class Backend(object):
+
+class Backend:
     """ Backend base class """
 
-    def __init__(self,id,config):
+    def __init__(self, id, config):
         self.id = id
         self.config = config
 
-    def update(self,updater):
+    # pylint: disable=no-self-use
+    def update(self, software):
         raise Exception("Not implemented")
 
-    def extract(self,xyz):
+    # pylint: disable=no-self-use
+    def extract(self):
         raise Exception("Not implemented")
 
-class Software(object):
+
+class Software:
     """ Software base class """
 
-    def __init__(self,id,config):
+    def __init__(self, id, config):
         self.id = id
         self.config = config
 
+    # pylint: disable=no-self-use
     def update(self):
         raise Exception("Not implemented")
-        
+
+
 class Output(threading.Thread):
     """ Output base class """
-        
-    def __init__(self,id,config):
-        super(Output,self).__init__()
+
+    def __init__(self, id, config):
+        super(Output, self).__init__()
         self.id = id
         self.config = config
 
         self.dataQueue = queue.Queue()
-        self.jobid = self.config.get(['options','jobid'])
+        self.jobid = self.config.get(["options", "jobid"])
 
     def run(self):
         while True:
@@ -173,42 +188,46 @@ class Output(threading.Thread):
                 self.dataQueue.task_done()
                 break
             try:
-                self.store({ data['id']: data['data'] })
-            except Exception as e:
-                logger.exception("Failed to store",e)
-            if 'type' in data and data['type'] == 'final':
+                self.store({data["id"]: data["data"]})
+            except Exception:
+                logger.exception("Failed to store")
+            if "type" in data and data["type"] == "final":
                 try:
-                    self.final({ data['id']: data['data'] })
-                except Exception as e:
-                    logger.exception("Failed to do self.final in %s" % self.id,e)
+                    self.final({data["id"]: data["data"]})
+                except Exception:
+                    logger.exception("Failed to do self.final in %s", self.id)
             self.dataQueue.task_done()
 
-        for t in range(int(self.config.get([self.id,'retry_count'],3))):
+        for _ in range(int(self.config.get([self.id, "retry_count"], 3))):
             try:
                 self.write()
                 break
-            except Exception as e:
-                logger.exception("Failed to do self.write in %s" % self.id,e)
-            time.sleep(int(self.config.get([self.id,'retry_sleep'],3)))
+            except Exception:
+                logger.exception("Failed to do self.write in %s", self.id)
+            time.sleep(int(self.config.get([self.id, "retry_sleep"], 3)))
 
-    def store(self,data):
+    # pylint: disable=no-self-use
+    def store(self, data):
         raise Exception("Not implemented")
 
-    def final(self,data):
+    def final(self, data):
         self.store(data)
 
+    # pylint: disable=no-self-use
     def write(self):
         raise Exception("Not implemented")
 
     def exit(self):
         self.dataQueue.put(None)
 
-class XMLWriter(object):
+
+class XMLWriter:
     """ XMLWriter base class """
 
-    def __init__(self,id,config):
+    def __init__(self, id, config):
         self.id = id
         self.config = config
 
-    def write(self,data):
+    # pylint: disable=no-self-use
+    def write(self, data):
         raise Exception("Not implemented")
--- sams/core.py
+++ sams/core.py
@@ -17,29 +17,35 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 """
+import logging
+import os
+import resource  # Resource usage information.
+import sys
+import threading
+
 import yaml
 
-import threading
 try:
     import queue
 except ImportError:
     import Queue as queue
-import os
 
-import logging
+
 logger = logging.getLogger(__name__)
+
 
 class Config:
     """ Config class, reads config_file.yaml """
-    
-    def __init__(self,config_file,extra={}):
+
+    def __init__(self, config_file, extra=None):
         self._cfg = {}
-        with open(config_file,"r") as file:
+        with open(config_file, "r") as file:
             self._cfg = yaml.load(file, Loader=yaml.SafeLoader)
 
-        self._cfg = self._merge(extra,self._cfg)
-    
-    def _merge(self,source, destination):
+        if extra:
+            self._cfg = self._merge(extra, self._cfg)
+
+    def _merge(self, source, destination):
         """ Merges two dicts """
         for key, value in source.items():
             if isinstance(value, dict):
@@ -51,42 +57,42 @@ class Config:
 
         return destination
 
-    def _get(self,cfg,items):
+    def _get(self, cfg, items):
         """ Helper function to do the recursive fetch of values from cfg """
         item = items[0]
         if item in cfg:
             if len(items) > 1:
-                return self._get(cfg[item],items[1:])
+                return self._get(cfg[item], items[1:])
             else:
                 return cfg[item]
         return None
 
-    def get(self,items,default=None):
+    def get(self, items, default=None):
         """ get config value """
-        value = self._get(self._cfg,items)
+        value = self._get(self._cfg, items)
         if value is None:
             return default
         return value
 
 
-class OneToN(threading.Thread):    
+class OneToN(threading.Thread):
     """ Class that takes one Queue and forwards into N other queues """
 
-    def __init__(self,id="OneToN"):
-        super(OneToN,self).__init__()
+    def __init__(self, id="OneToN"):
+        super(OneToN, self).__init__()
         self.id = id
-        
+
         self.inQueue = queue.Queue()
         self.outQueue = []
         self._lock = threading.Lock()
-        
+
         self.start()
 
     def run(self):
         """ Thread that forwards data from inQueue to outQueue[] """
         while True:
             value = self.inQueue.get()
-            logger.debug("%s received: %s" % (self.id,value))
+            logger.debug("%s received: %s", self.id, value)
             if value is None:
                 break
             # Lock the outQueue so that is will not change during sending.
@@ -96,45 +102,48 @@ class OneToN(threading.Thread):
             self._lock.release()
             self.inQueue.task_done()
 
-        logger.debug("%s is waiting for outQueue to be done" % self.id)
+        logger.debug("%s is waiting for outQueue to be done", self.id)
         for q in self.outQueue:
             q.join()
-        logger.debug("%s is done" % self.id)
+        logger.debug("%s is done", self.id)
 
-    def addQueue(self,queue):
+    def addQueue(self, queue):
         """ Add an new output queue """
         # Lock the outQueue list so that it is consistent
         self._lock.acquire()
         self.outQueue.append(queue)
         self._lock.release()
 
-    def put(self,value):
+    def put(self, value):
         """ Put value into the queue """
-        logger.debug("%s put(%s)" % (self.id,value))
+        logger.debug("%s put(%s)", self.id, value)
         self.inQueue.put(value)
 
     def exit(self):
         """ Enters an None value into the queue to exit """
-        logger.debug("%s got exit message" % self.id)
+        logger.debug("%s got exit message", self.id)
         self.inQueue.put(None)
         self.join()
+
 
 class ClassLoader:
     """ Static class that loads an class by name """
 
     @staticmethod
-    def load(package,class_name):
-        module = __import__(package,globals(),locals(),[class_name])
-        new_class = getattr(module,class_name)
+    def load(package, class_name):
+        module = __import__(package, globals(), locals(), [class_name])
+        new_class = getattr(module, class_name)
         return new_class
+
 
 # The standard I/O file descriptors are redirected to /dev/null by default.
 if hasattr(os, "devnull"):
-   REDIRECT_TO = os.devnull
+    REDIRECT_TO = os.devnull
 else:
-   REDIRECT_TO = "/dev/null"
+    REDIRECT_TO = "/dev/null"
 
-def createDaemon(umask = 0, workdir = "/", maxfds = 1024):
+
+def createDaemon(umask=0, workdir="/", maxfds=1024):
     """Detach a process from the controlling terminal and run it in the
     background as a daemon.
     """
@@ -142,27 +151,26 @@ def createDaemon(umask = 0, workdir = "/", maxfds = 1024):
     try:
         pid = os.fork()
     except OSError as e:
-        raise Exception("%s [%d]" % (e.strerror, e.errno))
+        raise Exception("%s [%d]" % (e.strerror, e.errno)) from e
 
-    if pid == 0:	# The first child.
+    if pid == 0:  # The first child.
         os.setsid()
 
         try:
-            pid = os.fork()	# Fork a second child.
+            pid = os.fork()  # Fork a second child.
         except OSError as e:
-            raise Exception("%s [%d]" % (e.strerror, e.errno))
+            raise Exception("%s [%d]" % (e.strerror, e.errno)) from e
 
-        if pid == 0:	# The second child.
+        if pid == 0:  # The second child.
             os.chdir(workdir)
             os.umask(umask)
         else:
-            os._exit(0)	# Exit parent (the first child) of the second child.
+            # Exit parent (the first child) of the second child.
+            os._exit(0)  # pylint: disable=protected-access
     else:
-        os._exit(0)	# Exit parent of the first child.
+        # Exit parent of the first child.
+        os._exit(0)  # pylint: disable=protected-access
 
-    #return(0)
-
-    import resource		# Resource usage information.
     maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
     if maxfd == resource.RLIM_INFINITY:
         maxfd = maxfds
@@ -171,52 +179,53 @@ def createDaemon(umask = 0, workdir = "/", maxfds = 1024):
     for fd in range(0, maxfd):
         try:
             os.close(fd)
-        except OSError as o:	# ERROR, fd wasn't open to begin with (ignored)
+        except OSError:  # ERROR, fd wasn't open to begin with (ignored)
             pass
 
     # This call to open is guaranteed to return the lowest file descriptor,
     # which will be 0 (stdin), since it was closed above.
-    os.open(REDIRECT_TO, os.O_RDWR)    # standard input (0)
+    os.open(REDIRECT_TO, os.O_RDWR)  # standard input (0)
 
     # Duplicate standard input to standard output and standard
     # error.
-    os.dup2(0, 1)            # standard output (1)
-    os.dup2(0, 2)         # standard error (2)
-    return(0)
+    os.dup2(0, 1)  # standard output (1)
+    os.dup2(0, 2)  # standard error (2)
+    return 0
+
 
 class JobSoftware:
-    def __init__(self,jobid,recordid):
+    def __init__(self, jobid, recordid):
         self._softwares = []
         self._jobid = jobid
         self._recordid = recordid
 
         if self._recordid is None:
             print("%s has no recordid" % (self._jobid))
-            exit()
+            sys.exit()
 
-    def addSoftware(self,software):
+    def addSoftware(self, software):
         if software.software is None:
             print("%s has no software" % (self._recordid))
-            exit()
+            sys.exit()
         if software.version is None:
             print("%s has no version" % (self._recordid))
-            exit()
+            sys.exit()
         if software.versionstr is None:
             print("%s has no versionstr" % (self._recordid))
-            exit()
+            sys.exit()
         if software.user_provided is None:
             print("%s has no user_provided" % (self._recordid))
-            exit()
+            sys.exit()
         if software.cpu is None:
             print("%s has no cpu" % (self._recordid))
-            exit()
+            sys.exit()
         self._softwares.append(software)
 
-    def softwares(self,remove_less_then=1.0):
+    def softwares(self, remove_less_then=1.0):
         t = self.total_cpu()
         if t == 0.0:
             return self._softwares
-        return [x for x in self._softwares if 100*x.cpu/t >= remove_less_then]
+        return [x for x in self._softwares if 100 * x.cpu / t >= remove_less_then]
 
     def jobid(self):
         return self._jobid
@@ -224,29 +233,41 @@ class JobSoftware:
     def recordid(self):
         return self._recordid
 
-    def total_cpu(self,remove_less_then=1.0):
+    def total_cpu(self, remove_less_then=1.0):
         t = sum([x.cpu for x in self._softwares])
         if t == 0.0 or remove_less_then == 0.0:
             return t
-        return sum([x.cpu for x in self._softwares if 100*x.cpu/t >= remove_less_then])
+        return sum(
+            [x.cpu for x in self._softwares if 100 * x.cpu / t >= remove_less_then]
+        )
 
     def __str__(self):
-        return "JobSoftware: %s (%s) - %s = %f" % (self._jobid, self._recordid, ",".join([x.__str__() for x in self._softwares]),self.total_cpu())
+        return "JobSoftware: %s (%s) - %s = %f" % (
+            self._jobid,
+            self._recordid,
+            ",".join([x.__str__() for x in self._softwares]),
+            self.total_cpu(),
+        )
 
     def __repr__(self):
         return self.__str__()
 
+
 class Software:
-    def __init__(self,software,version,versionstr,user_provided,cpu):
+    def __init__(self, software, version, versionstr, user_provided, cpu):
         self.software = software
         self.version = version
         self.versionstr = versionstr
         self.user_provided = user_provided
         self.cpu = cpu
 
-    
     def __str__(self):
-        return "Software: %s (%s/%s) = %f" % (self.software, self.version, self.versionstr, self.cpu)
+        return "Software: %s (%s/%s) = %f" % (
+            self.software,
+            self.version,
+            self.versionstr,
+            self.cpu,
+        )
 
     def __repr__(self):
         return self.__str__()
