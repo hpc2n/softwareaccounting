b316b68b6164cb5260cc7121d2d605313f4e0a77
Magnus Jonsson <magnus@hpc2n.umu.se>
2022-01-25 15:45:47 +0100

    Modules updated with isort/black/pylint.

--- sams/loader/File.py
+++ sams/loader/File.py
@@ -18,88 +18,103 @@ You should have received a copy of the GNU General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 """
 
+import json
+import logging
 import os
 import re
-import json
 
 import sams.base
 
-import logging
 logger = logging.getLogger(__name__)
 
+
 class Loader(sams.base.Loader):
-    
-    def __init__(self,id,config):
-        super(Loader,self).__init__(id,config)
-        self.in_path = self.config.get([self.id,'in_path'])
-        self.archive_path = self.config.get([self.id,'archive_path'])
-        self.error_path = self.config.get([self.id,'error_path'])
-        self.file_pattern = re.compile(self.config.get([self.id,'file_pattern'],"^.*$"))
+    def __init__(self, id, config):
+        super(Loader, self).__init__(id, config)
+        self.in_path = self.config.get([self.id, "in_path"])
+        self.archive_path = self.config.get([self.id, "archive_path"])
+        self.error_path = self.config.get([self.id, "error_path"])
+        self.file_pattern = re.compile(
+            self.config.get([self.id, "file_pattern"], "^.*$")
+        )
         self.files = []
         self.current_file = None
 
     def load(self):
         """ Find files in in_path matching file_pattern """
-        for root, dirs, files in os.walk(self.in_path):
+        for root, _, files in os.walk(self.in_path):
             for file in files:
                 logger.debug("Found file: %s", file)
                 if self.file_pattern.match(file):
-                    logger.debug("Add %s to files[]" % os.path.join(root,file))
-                    self.files.append({
-                        'file': file,
-                        'path': os.path.relpath(root,self.in_path)
-                    })
-    
+                    logger.debug("Add %s to files[]", os.path.join(root, file))
+                    self.files.append(
+                        {"file": file, "path": os.path.relpath(root, self.in_path)}
+                    )
+
     def next(self):
         if len(self.files) == 0:
-            return None            
+            return None
         self.current_file = self.files[0]
         self.files = self.files[1:]
         logger.debug(self.current_file)
-        filename = os.path.join(self.in_path,self.current_file['path'],self.current_file['file'])
+        filename = os.path.join(
+            self.in_path, self.current_file["path"], self.current_file["file"]
+        )
         try:
-            with open(filename,"r") as file:
+            with open(filename, "r") as file:
                 return json.load(file)
         except Exception as e:
-            raise Exception("Failed to load: %s due to %s" % (filename,str(e)))
+            raise Exception("Failed to load: %s due to %s" % (filename, str(e))) from e
         return None
 
     def error(self):
         """ move file from in_path -> error_path/ """
-        logger.info("Error: %s" % os.path.join(self.current_file['path'],self.current_file['file']))
+        logger.info(
+            "Error: %s",
+            os.path.join(self.current_file["path"], self.current_file["file"]),
+        )
 
-        out_path = os.path.join(self.error_path,self.current_file['path'])
+        out_path = os.path.join(self.error_path, self.current_file["path"])
         if not os.path.isdir(out_path):
             try:
                 os.makedirs(out_path)
-            except Exception as err:
+            except Exception:
                 # Handle possible raise from other process
                 if not os.path.isdir(out_path):
                     assert False, "Failed to makedirs '%s' " % out_path
 
         # Rename file to error directory
-        os.rename(os.path.join(self.in_path,self.current_file['path'],self.current_file['file']),
-                  os.path.join(out_path,self.current_file['file']))
+        os.rename(
+            os.path.join(
+                self.in_path, self.current_file["path"], self.current_file["file"]
+            ),
+            os.path.join(out_path, self.current_file["file"]),
+        )
 
         self.current_file = None
-        pass
 
     def commit(self):
         """ move file from in_path -> archive_path/ """
-        logger.info("Commit: %s" % os.path.join(self.current_file['path'],self.current_file['file']))
+        logger.info(
+            "Commit: %s",
+            os.path.join(self.current_file["path"], self.current_file["file"]),
+        )
 
-        out_path = os.path.join(self.archive_path,self.current_file['path'])
+        out_path = os.path.join(self.archive_path, self.current_file["path"])
         if not os.path.isdir(out_path):
             try:
                 os.makedirs(out_path)
-            except Exception as err:
+            except Exception:
                 # Handle possible raise from other process
                 if not os.path.isdir(out_path):
                     assert False, "Failed to makedirs '%s' " % out_path
 
         # Rename file to archive directory
-        os.rename(os.path.join(self.in_path,self.current_file['path'],self.current_file['file']),
-                  os.path.join(out_path,self.current_file['file']))
+        os.rename(
+            os.path.join(
+                self.in_path, self.current_file["path"], self.current_file["file"]
+            ),
+            os.path.join(out_path, self.current_file["file"]),
+        )
 
         self.current_file = None
-        pass
