676bb3ac3fdb3b69d69ea7760ff9d0d8a3274dc5
Magnus Jonsson <magnus@hpc2n.umu.se>
2022-02-04 14:04:14 +0100

    Release of version 1.5

--- sams/__init__.py
+++ sams/__init__.py
@@ -16,4 +16,4 @@ You should have received a copy of the GNU General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "master"
+__version__ = "1.5"
