22406bff7c3c3da374a8c791fcc5a50dd108e8f6
Magnus Jonsson <magnus@hpc2n.umu.se>
2022-03-29 10:53:50 +0200

    Add FileSlurmInfoFallback loader module
    Add support for fetch sams.sampler.SlurmInfo from sacct
    during sams-aggregator.

---
+++ docs/loader/FileSlurmInfoFallback.md
@@ -0,0 +1,39 @@
+# sams.loader.FileSlurmInfoFallback
+
+Extends sams.loader.File but will use sacct to fetch data 
+from slurmdbd if sams.sampler.SlurmInfo is not used or 
+failes to fetch data.
+
+Used by the [*sams-aggregator*](../sams-aggregator.md)
+
+# Config options
+
+Same options as sams.loader.File but extends with the following
+
+## sacct
+
+Path where sacct binary is located
+
+Default: "/usr/bin/sacct"
+
+## environment
+
+Extra environtment for command.
+
+An hash of key-value with envname-value.
+
+Can for example be used to set the TZ option to get output in UTC.
+
+
+# Example configuration
+
+```
+sams.loader.FileSlurmInfoFallback:
+  in_path: /data/softwareaccounting/data
+  archive_path: /data/softwareaccounting/archive
+  error_path: /data/softwareaccounting/error
+  file_pattern: '^.*\.json$'
+  sacct: /usr/bin/sacct
+  environment:
+    TZ=UTC
+```
---
+++ sams/loader/FileSlurmInfoFallback.py
@@ -0,0 +1,81 @@
+"""
+FileSlurmInfoFallback Loader
+
+SAMS Software accounting
+Copyright (C) 2018-2021  Swedish National Infrastructure for Computing (SNIC)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+import os
+import subprocess
+
+from sams.loader.File import Loader as File
+
+logger = logging.getLogger(__name__)
+
+
+class SacctLoader:
+    def __init__(self, sacct, env):
+        self.sacct = sacct
+        self.env = env
+
+    def run(self, jobid):
+        local_env = os.environ.copy()
+        for k, v in self.env.items():
+            local_env[k] = v
+
+        process = subprocess.Popen(
+            [
+                self.sacct,
+                "-P",
+                "-j",
+                str(jobid),
+                "-X",
+                "--noheader",
+                "-o",
+                "Account,Start,User,NNodes,NCPU,Partition,UID",
+            ],
+            env=local_env,
+            stdout=subprocess.PIPE,
+        )
+        (account, starttime, username, nodes, cpus, partition, uid) = (
+            process.stdout.readline().decode("ascii").strip().split("|")
+        )
+        return dict(
+            account=account,
+            starttime=starttime,
+            username=username,
+            nodes=nodes,
+            cpus=cpus,
+            partition=partition,
+            uid=uid,
+        )
+
+
+class Loader(File):
+    def __init__(self, id, config):
+        super(Loader, self).__init__(id, config)
+        self.sacct = self.config.get([self.id, "sacct"], "/usr/bin/sacct")
+        self.env = self.config.get([self.id, "environment"], {})
+
+    def next(self):
+        data = super(Loader, self).next()
+        if not data.get("sams.sampler.SlurmInfo", {}):
+            jobid = int(data["sams.sampler.Core"]["jobid"])
+            sacct = SacctLoader(self.sacct, self.env)
+            data["sams.sampler.SlurmInfo"] = sacct.run(jobid)
+
+        return data
