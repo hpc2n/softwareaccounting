0f4067fa46937aaca7a42da1274391325848c0a4
Leonard Nielsen <leoniel@chalmers.se>
2024-06-14 15:01:58 +0200

    updated FSStats

--- sams/sampler/FSStats.py
+++ sams/sampler/FSStats.py
@@ -42,6 +42,8 @@ import glob
 import logging
 import os
 
+from typing import Dict
+
 import sams.base
 
 logger = logging.getLogger(__name__)
@@ -51,20 +53,17 @@ class FSStats:
     def __init__(self, mount_points):
         self.mount_points = mount_points
 
-    def sample(self):
-        ret = {}
+    def get_sample(self) -> Dict:
+        ret = dict()
         for mp in self.mount_points:
             try:
                 statvfs = os.statvfs(mp)
-                ret[mp] = dict(
-                    size=statvfs.f_frsize
-                    * statvfs.f_blocks,  # Size of filesystem in bytes
-                    free=statvfs.f_frsize
-                    * statvfs.f_bavail,  # Number of free bytes that ordinary users
-                    # are allowed to use (excl. reserved space)
-                    used=statvfs.f_frsize * statvfs.f_blocks
-                    - statvfs.f_frsize * statvfs.f_bavail,
-                )
+                size = statvfs.f_frsize * statvfs.f_blocks
+                free = statvfs.f_frsize * statvfs.f_bavail
+                ret[mp] = dict(size=size,  # Size of filesystem in bytes
+                               free=free,  # Number of free bytes that ordinary users
+                                           # are allowed to use (excl. reserved space)
+                               used=size - free)
             except Exception as e:
                 logger.error(e)
         return ret
@@ -73,32 +72,31 @@ class FSStats:
 class Sampler(sams.base.Sampler):
     def __init__(self, id, outQueue, config):
         super(Sampler, self).__init__(id, outQueue, config)
-        self.processes = {}
-        self.sampler_interval = self.config.get([self.id, "sampler_interval"], 60)
-        self.mount_points = self.config.get([self.id, "mount_points"])
-        self.jobid = self.config.get(["options", "jobid"], 0)
-
+        self.processes = dict()
+        self.sampler_interval = self.config.get([self.id, 'sampler_interval'], 60)
+        self.mount_points = self.config.get([self.id, 'mount_points'])
+        self.jobid = self.config.get(['options', 'jobid'], 0)
         if not self.mount_points:
-            raise sams.base.SamplerException("mount_points not configured")
-
+            raise sams.base.SamplerException('mount_points not configured')
         mps = []
         for mp in self.mount_points:
             mps += glob.glob(mp % dict(jobid=self.jobid))
-
         self.fsstat = None
         if mps:
             self.fsstat = FSStats(mps)
 
-    def do_sample(self):
+    def do_sample(self) -> bool:
         if not self.fsstat:
             return False
         return True
 
-    def sample(self):
-        logger.debug("sample()")
+    def sample(self) -> None:
+        logger.debug('sample()')
         if self.fsstat:
-            self.store(self.fsstat.sample())
+            sample = self.fsstat.get_sample()
+            self.store(sample)
+            self._most_recent_sample = self.storage_wrapper(sample)
 
-    @classmethod
-    def final_data(cls):
-        return {}
+    @staticmethod
+    def final_data() -> Dict:
+        return dict()
